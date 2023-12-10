600012faed986439f6f6a847daeebeaf8cdda9ea
CLOG9 <cakipan677@gmail.com>
2023-12-10 03:19:50 +0100

    fix (ioredis): configure ioredis with docker compose

--- src/config/redis-client.ts
+++ src/config/redis-client.ts
@@ -1,5 +1,8 @@
 import Redis from "ioredis";
 
-const redis = new Redis();
+const redis = new Redis({
+    host: 'prisma-redis', // Docker Compose service name
+    port: 6379,            // Docker Compose port
+  });
 
 export default redis;
