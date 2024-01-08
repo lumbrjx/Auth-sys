4e20a5659c08caf705aa8631dffbfa2b12c6bbbf
CLOG9 <cakipan677@gmail.com>
2024-01-08 08:32:16 +0100

    dd

--- src/config/redis-client.ts
+++ src/config/redis-client.ts
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST as string, // Docker Compose service name
+  host: process.env.REDIS_HOST as string, // service name
   port: process.env.REDIS_PORT as unknown as number, // Docker Compose port
 });
 
