e28a789f2cda8fdf020672809914477bfcc77863
CLOG9 <cakipan677@gmail.com>
2024-01-29 18:59:00 +0100

    update env

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -8,6 +8,6 @@ interface AppConfig {
 export const environments: AppConfig = {
   apiUrl: process.env.API_URL as string,
   dbUrl: process.env.VITE_PG_DATABASE as string,
-  schema: process.env.DB_SCHEMA_PATH as string,
-  out: process.env.DB_OUT_PATH,
+  schema: process.env.VITE_DB_SCHEMA_PATH as string,
+  out: process.env.VITE_DB_OUT_PATH,
 };
