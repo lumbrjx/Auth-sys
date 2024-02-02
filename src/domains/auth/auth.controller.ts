750034e704f3f8c680bd6e926de4e3c3d268fa9f
CLOG9 <cakipan677@gmail.com>
2024-02-02 19:04:01 +0100

    update (nginx): deploy to nginx

--- src/domains/auth/auth.controller.ts
+++ src/domains/auth/auth.controller.ts
@@ -9,93 +9,93 @@ import { validateUser } from "./auth.validator";
 import { ZodError } from "zod";
 // Login
 export async function loginController(
-  req: FastifyRequest,
-  reply: FastifyReply
+	req: FastifyRequest,
+	reply: FastifyReply
 ) {
-  try {
-    const parsedBody = LoginSchema.parse(req.body);
-    const userResult = await getUser(parsedBody.email, false);
-    const existingUser = await validateUser(userResult, parsedBody, reply);
-    if (existingUser?.TWO_FA === true) {
-      const tfaToken = generate2FACode(parsedBody);
-      return reply
-        .code(200)
-        .send({ ok: true, message: "2FA code is sent " + (await tfaToken) });
-    }
-    req.session.email = parsedBody.email;
-    req.session.authenticated = true;
-    // await createRedisSession(req, existingUser);
+	try {
+		const parsedBody = LoginSchema.parse(req.body);
+		const userResult = await getUser(parsedBody.email, false);
+		const existingUser = await validateUser(userResult, parsedBody, reply);
+		if (existingUser?.TWO_FA === true) {
+			const tfaToken = generate2FACode(parsedBody);
+			return reply
+				.code(200)
+				.send({ ok: true, message: "2FA code is sent " + (await tfaToken) });
+		}
+		req.session.email = parsedBody.email;
+		req.session.authenticated = true;
+		// await createRedisSession(req, existingUser);
 
-    return reply.status(200).send({ ok: true, message: "Authorized" });
-  } catch (error: any) {
-    if (error instanceof ZodError) {
-      return reply.status(400).send({ error: error.issues[0].message });
-    } else {
-      return reply.status(500).send({ error: "Internal Server Error" });
-    }
-  }
+		return reply.status(200).send({ ok: true, message: "Authorized" });
+	} catch (error: any) {
+		if (error instanceof ZodError) {
+			return reply.status(400).send({ error: error.issues[0].message });
+		} else {
+			return reply.status(500).send({ error: "Internal Server Error" });
+		}
+	}
 }
 // 2FA post code generation
 export async function tfagenController(
-  req: FastifyRequest,
-  reply: FastifyReply
+	req: FastifyRequest,
+	reply: FastifyReply
 ) {
-  try {
-    const parsedBody = tfacodeSchema.parse(req.body);
-    const { user } = (await req.params) as any;
-    if (!user) {
-      reply.code(400).send("Error logging in please try again");
-    }
-    const tfauser = await redis.get(csts.TWO_FACTOR_AUTH + user);
-    if (tfauser === null) {
-      return reply.code(401).send("Unauthorized");
-    }
-    if (tfauser.trim() !== parsedBody.code.trim()) {
-      return reply.code(401).send("invalid 2fa code");
-    }
-    const userResult = await getUser(user.trim(), false);
-    if (!userResult.success) {
-      reply.code(401).send(userResult.error ?? "Invalid passowrd or email");
-    }
+	try {
+		const parsedBody = tfacodeSchema.parse(req.body);
+		const { user } = (await req.params) as any;
+		if (!user) {
+			reply.code(400).send("Error logging in please try again");
+		}
+		const tfauser = await redis.get(csts.TWO_FACTOR_AUTH + user);
+		if (tfauser === null) {
+			return reply.code(401).send("Unauthorized");
+		}
+		if (tfauser.trim() !== parsedBody.code.trim()) {
+			return reply.code(401).send("invalid 2fa code");
+		}
+		const userResult = await getUser(user.trim(), false);
+		if (!userResult.success) {
+			reply.code(401).send(userResult.error ?? "Invalid passowrd or email");
+		}
 
-    await redis.del(csts.TWO_FACTOR_AUTH + user);
-    req.session.email = user;
-    req.session.authenticated = true;
-    return reply.status(200).send({ ok: true, message: "Authorized" });
-  } catch (error: any) {
-    if (error instanceof ZodError) {
-      return reply.status(400).send({ error: error.issues[0].message });
-    } else {
-      return reply.status(500).send({ error: "Internal Server Error" });
-    }
-  }
+		await redis.del(csts.TWO_FACTOR_AUTH + user);
+		req.session.email = user;
+		req.session.authenticated = true;
+		return reply.status(200).send({ ok: true, message: "Authorized" });
+	} catch (error: any) {
+		if (error instanceof ZodError) {
+			return reply.status(400).send({ error: error.issues[0].message });
+		} else {
+			return reply.status(500).send({ error: "Internal Server Error" });
+		}
+	}
 }
 // Register
 export async function reigsterController(
-  req: FastifyRequest,
-  reply: FastifyReply
+	req: FastifyRequest,
+	reply: FastifyReply
 ) {
-  try {
-    const parsedBody = RegisterSchema.parse(req.body);
-    const userResult = await getUser(parsedBody.email, false);
-    if (userResult.data?.email === parsedBody.email) {
-      return reply.code(400).send({
-        ok: false,
-        error: userResult.error ?? "cannot create a user",
-      });
-      // errGen(401, userResult.error?.toString() ?? "Invalid passowrd or email");
-    }
-    bcrypt.hash(parsedBody.password, 10, async (err: any, hash: string) => {
-      await createUser({ ...parsedBody, password: hash });
-    });
+	try {
+		const parsedBody = RegisterSchema.parse(req.body);
+		const userResult = await getUser(parsedBody.email, false);
+		if (userResult.data?.email === parsedBody.email) {
+			return reply.code(400).send({
+				ok: false,
+				error: userResult.error ?? "cannot create a user",
+			});
+			// errGen(401, userResult.error?.toString() ?? "Invalid passowrd or email");
+		}
+		bcrypt.hash(parsedBody.password, 10, async (err: any, hash: string) => {
+			await createUser({ ...parsedBody, password: hash });
+		});
 
-    reply.code(201).send({ ok: true, message: "created" });
-  } catch (error: any) {
-    if (error instanceof ZodError) {
-      reply.status(400).send({ error: error.issues[0].message });
-    } else {
-      reply.status(500).send({ error: "Internal Server Error" });
-    }
-  }
+		reply.code(201).send({ ok: true, message: "created" });
+	} catch (error: any) {
+		if (error instanceof ZodError) {
+			reply.status(400).send({ error: error.issues[0].message });
+		} else {
+			reply.status(500).send({ error: "Internal Server Error" });
+		}
+	}
 }
 // OAUTH2, 2fa-, cookie
