9acabe25c5e37acaa8ea2ceb8acea3fe268ea68c
CLOG9 <cakipan677@gmail.com>
2024-02-02 19:25:23 +0100

    update

--- src/config/redis-client.ts
+++ src/config/redis-client.ts
@@ -2,10 +2,10 @@ import RedisStore from "connect-redis";
 import Redis from "ioredis";
 
 export const redis = new Redis({
-  host: process.env.REDIS_HOST as string, // service name
-  port: process.env.REDIS_PORT as unknown as number, //  port
-  enableAutoPipelining: true,
+	host: process.env.VITE_REDIS_HOST as string, // service name
+	port: process.env.VITE_REDIS_PORT as unknown as number, //  port
+	enableAutoPipelining: true,
 });
 export const store = new RedisStore({
-  client: redis,
+	client: redis,
 });
